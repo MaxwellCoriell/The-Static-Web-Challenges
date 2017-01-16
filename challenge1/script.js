6104af50257fad8307091653f720c18e458b2514
Maxwell Baldridge <coolhandmax@Maxwells-MacBook-Pro.local>
2017-01-16 04:54:12 -0600

    completed individual challenge 1

---
+++ challenge1/script.js
@@ -0,0 +1,6 @@
+// grab the input box ('slider_amount') and set the value property to that of 'slideAmount'
+// slideAmount is grabbed from "this.value" in a onchange="rangeBarToForm(this.value)
+
+function rangeBarToForm (slideAmount) {
+	document.getElementById("slider_value").value = slideAmount;
+}
